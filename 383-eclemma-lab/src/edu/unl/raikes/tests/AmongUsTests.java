c141027d70226ed9ce930f4ac43c7f3c224c555b
antoniolinhart <antonio.linhart@gmail.com>
2021-02-13 12:52:13 -0600

    Moved tests to AmongUsTests.java and made placeholder tests

---
+++ 383-eclemma-lab/src/edu/unl/raikes/tests/AmongUsTests.java
@@ -0,0 +1,125 @@
+package edu.unl.raikes.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.unl.raikes.amongus.Lobby;
+import edu.unl.raikes.amongus.Player;
+import edu.unl.raikes.amongus.SuspiciousCalculator;
+
+public class AmongUsTests {
+
+	/** Do NOT create any more tests beyond the scope of this outline.
+	 * Refer to the lab README for more details on what constitutes a valid test.
+	 * There is no need to rename tests for this lab.
+	 */
+	
+	@Test
+	public void exampleTest() {
+		Player player = null;
+		Lobby lobby = null;
+		int suspiciousScore = SuspiciousCalculator.getSuspiciousMetric(player, lobby);
+
+		Assert.assertEquals(-1, suspiciousScore);
+	}
+	
+	@Test
+	public void test1() {
+		
+	}
+	
+	@Test
+	public void test2() {
+		
+	}
+	
+	@Test
+	public void test3() {
+		
+	}
+	
+	@Test
+	public void test4() {
+		
+	}
+	
+	@Test
+	public void test5() {
+		
+	}
+	
+	@Test
+	public void test6() {
+		
+	}
+	
+	@Test
+	public void test7() {
+		
+	}
+	
+	@Test
+	public void test8() {
+		
+	}
+	
+	@Test
+	public void test9() {
+		
+	}
+	
+	@Test
+	public void test10() {
+		
+	}
+	
+	@Test
+	public void test11() {
+		
+	}
+	
+	@Test
+	public void test12() {
+		
+	}
+	
+	@Test
+	public void test13() {
+		
+	}
+	
+	@Test
+	public void test14() {
+		
+	}
+	
+	@Test
+	public void test15() {
+		
+	}
+	
+	@Test
+	public void test16() {
+		
+	}
+	
+	@Test
+	public void test17() {
+		
+	}
+	
+	@Test
+	public void test18() {
+		
+	}
+	
+	@Test
+	public void test19() {
+		
+	}
+	
+	@Test
+	public void test20() {
+		
+	}
+}
